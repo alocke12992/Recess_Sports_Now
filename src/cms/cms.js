24ee9c368f9b4e9fca090eb65f916e8cd118d7c5
Andrew Locke <alocke12992@gmail.com>
2018-07-18 08:54:15 -0700

    Builds ad preview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,8 +3,10 @@ import CMS from 'netlify-cms'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
+import BackgroundAdPreview from './preview-templates/BackgroundAdPreview'
 
 CMS.registerPreviewStyle('/styles.css')
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewTemplate('background', BackgroundAdPreview)
