22a836b392ff3e367c960425390fd4e92d6a4feb
Andrew Locke <alocke12992@gmail.com>
2018-08-04 11:48:15 -0700

    refactor template wrapper

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,38 +1,13 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import {AdContext} from '../components/AdContext';
 import Login from '../components/Login';
-import Navbar from '../components/Navbar';
 import './all.sass';
 
 
 class TemplateWrapper extends React.Component {
   state = {password: 'Playground2018', isAuthenticated: false}
-
-  showWebsite = () => {
-    const {children, data} = this.props
-    console.log(data)
-    const {edges: ads} = data.allMarkdownRemark
-    const headerAd = ads.filter(ad => ad.node.frontmatter.templateKey === "headerAd")
-    return (
-      <section className="section" >
-        <Helmet title="Recess Sports Now" />
-        <div className="columns">
-          <div className="column is-one-fifth" style={{background: "blue"}}>
-          </div>
-          <div className="column is-three-fifths">
-            <Navbar headerAd={headerAd[0].node.frontmatter.image} />
-            <AdContext.Provider value={ads.filter(ad => ad.node.frontmatter.templateKey !== "headerAd")}>
-              <div>{children()}</div>
-            </AdContext.Provider>
-          </div>
-          <div className="column is-one-fifth" style={{background: "blue"}}>
-          </div>
-        </div>
-      </section>
-    )
-  }
 
   toggleForm = () => {
     this.setState((state) => {
@@ -40,13 +15,16 @@ class TemplateWrapper extends React.Component {
     })
   }
 
-
   render() {
+    const {children, data} = this.props
+    const {edges: ads} = data.allMarkdownRemark
     return (
-      <section className="section" >
+      <Fragment>
         <Helmet title="Recess Sports Now" />
-        {this.showWebsite()}
-      </section>
+        <AdContext.Provider value={ads}>
+          <div>{children()}</div>
+        </AdContext.Provider>
+      </Fragment>
     )
   }
 }
