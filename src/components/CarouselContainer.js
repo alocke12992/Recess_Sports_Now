b3d8e5ebc9ca7bbcfe4429a417712091afac03b1
Andrew Locke <alocke12992@gmail.com>
2018-08-14 08:12:16 -0700

    Formats carousel

--- src/components/CarouselContainer.js
+++ src/components/CarouselContainer.js
@@ -3,33 +3,33 @@ import Link from 'gatsby-link';
 import Swiper from 'react-id-swiper';
 import CarouselSlide from './CarouselSlide';
 
-class CarouselContainer extends React.Component {
-  render() {
-    const posts = this.props.posts
-    const params = {
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clickable: true
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-      },
-      spaceBetween: 30
-    }
-    return (
-      <Swiper {...params}>
-        {posts.map(({node: post}) => (
-          <div>
-            <Link to={post.fields.slug}>
-              <img src={post.frontmatter.featuredImage} style={{height: '100%', width: '100%'}} />
-            </Link>
-          </div>
-        ))}
-      </Swiper>
-    )
+const CarouselContainer = (props) => {
+  const posts = props.posts
+  const params = {
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    spaceBetween: 30,
+    autoplay: {
+      delay: 1000,
+    },
+    loop: true,
   }
+  return (
+    <Swiper {...params}>
+      {posts.map(({node: post}) => (
+        <div className="carouselSlides">
+          <CarouselSlide post={post} />
+        </div>
+      ))}
+    </Swiper>
+  )
 }
 
 export default CarouselContainer
