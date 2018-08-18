3735bfcd6f73a7b4fe1ed9ce44619a9704805e44
Andrew Locke <alocke12992@gmail.com>
2018-08-18 16:30:29 -0700

    fix nav on scroll

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Link from 'gatsby-link';
 import {kebabCase} from 'lodash';
-import {Icon, Button, Navbar, NavbarStart, NavbarMenu, NavbarDropdown, NavbarItem, NavbarBurger} from 'bloomer';
+import {Icon, Button, Navbar, NavbarStart, NavbarMenu, NavbarBrand, NavbarItem, NavbarBurger} from 'bloomer';
 
 const tags = [
   "ENTERTAINMENT",
   "BUSINESS",
   "TECH",
   "LIFESTYLE",
+  "PODCASTS",
+  "ORIGINALS"
 ]
 
 class NavMenu extends React.Component {
@@ -18,57 +20,70 @@ class NavMenu extends React.Component {
       return {isActive: !state.isActive}
     })
     )
-
   }
 
   render() {
     return (
       <Navbar>
+        {
+          this.props.location < 1 ?
+            <NavbarBrand>
+              <NavbarItem>
+                <Link to="/">
+                  <img src={this.props.logo} style={{height: '100px', width: 'auto'}} />
+                </Link>
+              </NavbarItem>
+              <NavbarItem isHidden='desktop'>
+                <Icon className='fa fa-github' />
+              </NavbarItem>
+              <NavbarItem isHidden='desktop'>
+                <Icon className='fa fa-twitter' style={{color: '#55acee'}} />
+              </NavbarItem>
+              <NavbarBurger isActive={this.state.isActive} onClick={this.onClickNav} />
+            </NavbarBrand>
+            :
+            null
+        }
         <NavbarBurger isActive={this.state.isActive} onClick={this.onClickNav} />
         <NavbarMenu isActive={this.state.isActive} onClick={this.onClickNav}>
           <NavbarStart>
-            <Link to="/">
-              HOME
-            </Link>
-            <NavbarItem hasDropdown isHoverable>
-              CATEGORIES
-              <NavbarDropdown>
-                {tags.map(tag => (
-                  <NavbarItem key={tag + 'tag'}><Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link></NavbarItem>
-                ))}
-              </NavbarDropdown>
-            </NavbarItem>
+            {tags.map(tag => (
+              <Link key={tag + 'tag'} to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+            ))}
             <Link to="/contact">
               CONTACT
             </Link>
-            <Link to="/contact">
-              ORIGINALS
-            </Link>
-            <Button onClick={() => this.props.toggleSearch()} className="searchButton">
-              <Icon isSize='small'>
-                <span className="fa fa-search" />
-              </Icon>
-            </Button>
-            <a rel="noopener noreferral" target="_blank">
-              <Icon isSize='small'>
-                <span className="fa fa-info-circle" />
-              </Icon>
-            </a>
-            <a href="https://www.facebook.com/pg/RecessSports/posts/?ref=page_internal" rel="noopener noreferral" target="_blank">
-              <Icon isSize='small'>
-                <span className="fa fa-facebook-f" />
-              </Icon>
-            </a>
-            <a href="https://twitter.com/Recesssportsnow" rel="noopener noreferral" target="_blank">
-              <Icon isSize='small'>
-                <span className="fa fa-twitter" />
-              </Icon>
-            </a>
-            <a href="https://www.instagram.com/recesssportsnow/" rel="noopener noreferral" target="_blank">
-              <Icon isSize='small'>
-                <span className="fa fa-instagram" />
-              </Icon>
-            </a>
+            {this.props.location < 1 ?
+              <React.Fragment>
+                <Button onClick={() => this.props.toggleSearch()} className="searchButton">
+                  <Icon isSize='small'>
+                    <span className="fa fa-search" />
+                  </Icon>
+                </Button>
+                <a rel="noopener noreferral" target="_blank">
+                  <Icon isSize='small'>
+                    <span className="fa fa-info-circle" />
+                  </Icon>
+                </a>
+                <a href="https://www.facebook.com/pg/RecessSports/posts/?ref=page_internal" rel="noopener noreferral" target="_blank">
+                  <Icon isSize='small'>
+                    <span className="fa fa-facebook-f" />
+                  </Icon>
+                </a>
+                <a href="https://twitter.com/Recesssportsnow" rel="noopener noreferral" target="_blank">
+                  <Icon isSize='small'>
+                    <span className="fa fa-twitter" />
+                  </Icon>
+                </a>
+                <a href="https://www.instagram.com/recesssportsnow/" rel="noopener noreferral" target="_blank">
+                  <Icon isSize='small'>
+                    <span className="fa fa-instagram" />
+                  </Icon>
+                </a>
+              </React.Fragment>
+              :
+              null
+            }
           </NavbarStart>
         </NavbarMenu>
       </Navbar>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,35 +4,61 @@ import Search from './Search';
 import NavMenu from './NavMenu';
 
 class NavbarComponent extends React.Component {
+  state = {location: null}
+  componentDidMount() {
+    this.getScroll()
+  }
+
+  getScroll = () => {
+    window.addEventListener('scroll', () => {
+      const navMenuNode = document.getElementById('navMenu').getClientRects()[0]
+      this.setState({location: navMenuNode.top})
+    })
+  }
 
   render() {
-    console.log(this.props)
+    const {location} = this.state
     return (
-      <div className="columns" style={{marginBottom: '0px'}}>
-        <div className="column is-8 is-offset-2 navWrapper">
-          <div>
-            <figure className="image is-3by1">
-              <img src={this.props.headerAd} style={{height: '250px'}} />
-            </figure>
-            <div className="columns" style={{paddingTop: '20px'}}>
-              <div className='column is-one-third is-offset-4'>
-                <figure className="image is-3by1">
-                  <img src={RsnLogo} />
-                </figure>
+      <div className="columns" style={{marginBottom: '0px'}} id="nav">
+        <div className="column is-12">
+          <div className="columns">
+            <div className="column is-8 is-offset-2 navWrapper">
+              <figure className="image is-3by1">
+                <img src={this.props.headerAd} style={{height: '250px'}} id="logo" />
+              </figure>
+              <div className="columns" style={{paddingTop: '20px'}}>
+                <div className='column is-one-third is-offset-4'>
+                  <figure className="image is-3by1">
+                    <img src={RsnLogo} />
+                  </figure>
+                </div>
+              </div>
+              <div className="columns" id="navMenu">
+                {
+                  location >= 0 ?
+                    <div className="column is-10 is-offset-1">
+                      <NavMenu toggleSearch={this.props.toggleSearch} location={location} />
+                      {
+                        this.props.showSearch ?
+                          <Search getSearch={this.props.getSearch} toggleSearch={this.props.toggleSearch} />
+                          :
+                          null
+                      }
+                    </div>
+                    :
+                    null
+                }
               </div>
             </div>
           </div>
-          <div className="columns">
-            <div className="column is-10 is-offset-1">
-              <NavMenu toggleSearch={this.props.toggleSearch} />
-              {
-                this.props.showSearch ?
-                  <Search getSearch={this.props.getSearch} toggleSearch={this.props.toggleSearch} />
-                  :
-                  null
-              }
-            </div>
-          </div>
+          {
+            location < 1 ?
+              <div className="fixedNav">
+                <NavMenu logo={RsnLogo} toggleSearch={this.props.toggleSearch} location={location} />
+              </div>
+              :
+              null
+          }
         </div>
       </div>
     )
